884b2ef51348a1c1c60082cb728ac8cf97cb6e30
Amna Syeda <amna.syeda98@gmail.com>
2021-09-15 09:54:52 -0400

    fixing errors in searchbooks.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,6 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
-  request: operation => {
-    const token = localStorage.getItem('id_token');
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    });
-  },
-  uri: '/graphql'
-});
-
 function App() {
   return (
     <ApolloProvider client={client}>
@@ -35,5 +22,18 @@ function App() {
     </ApolloProvider>
   );
 }
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 export default App;
